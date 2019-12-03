7e2e217d85f6ee080a9a96714f082409d7ae3588
Tom <798317108@qq.com>
2019-12-03 18:12:48 +0800

    初始项目框架

---
+++ Source/Robomb/Public/ Blueprints/RobombGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "RobombGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOMB_API URobombGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Robomb/Public/ Blueprints/RobombGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "RobombGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOMB_API ARobombGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
